810e73d2e34fc394058bee5e42c68706630df870
janesenaj42 <janesoopy@gmail.com>
2025-04-15 23:35:59 +0800

    feat: add rsocket authentication

--- README.md
+++ README.md
@@ -5,7 +5,17 @@ To start the backend,
 ```
 gradlew bootrun
 ```
-## DevContainers
+
+### Setup
+This project uses keycloak for authentication. To set up keycloak, run
+```declarative
+docker compose up -d
+```
+Keycloak will be available at http://localhost:8080
+
+(TODO: include keycloak setup guide)
+
+### DevContainers
 If you're lazy to install Java, you may run the project in VS Code using DevContainers extention with the given `Dockerfile`.
 
 
@@ -19,17 +29,35 @@ curl --location 'http://localhost:8080/notifications' \
 
 ### RSocket
 1. Download `rsc` from https://github.com/making/rsc.
+2. RSocket endpoints requires authencation. Get the bearer token using the following curl command, updating `KEYCLOAK_URL`, `REALM_NAME`, `CLIENT_ID`, `USERNAME` and `PASSWORD` as required.
+   ```
+   export KEYCLOAK_URL=http://localhost:8080
+   export REALM_NAME=my-realm
+   export CLIENT_ID=my-sample-app
+   export USERNAME=asdf
+   export PASSWORD=asdfasdf
+   
+   ACCESS_TOKEN=$(curl -s --location "$KEYCLOAK_URL/realms/$REALM_NAME/protocol/openid-connect/token" \
+   --header "Content-Type: application/x-www-form-urlencoded" \
+   --data-urlencode "grant_type=password" \
+   --data-urlencode "client_id=$CLIENT_ID" \
+   --data-urlencode "username=$USERNAME" \
+   --data-urlencode "password=$PASSWORD" \
+    | grep -o '"access_token":"[^"]*"' | cut -d':' -f2 | tr -d '"' ) && echo $ACCESS_TOKEN
+   ```
+   
 2. Test the respective rSocket endpoints:
     ```
     // Request-response
-   rsc --request --route=/request-response --data="my sample input" tcp://localhost:7000
+   rsc --request --route=/request-response --authBearer=$ACCESS_TOKEN --data="my sample input" tcp://localhost:7000
    
    // Fire-forget
-   rsc --request --route=/fire-forget --data="my sample input" tcp://localhost:7000 
+   rsc --request --route=/fire-forget --authBearer=$ACCESS_TOKEN --data="my sample input" tcp://localhost:7000 
        
     // Request-stream
-   rsc --stream --route=/request-stream --data="my sample input"  tcp://localhost:7000
+   rsc --stream --route=/request-stream --authBearer=$ACCESS_TOKEN --data="my sample input"  tcp://localhost:7000
     ```
+   
 
 
 
---
+++ src/main/java/com/example/demo/config/RSocketSecurityConfig.java
@@ -0,0 +1,68 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.rsocket.EnableRSocketSecurity;
+import org.springframework.security.config.annotation.rsocket.RSocketSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtReactiveAuthenticationManager;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.rsocket.core.PayloadSocketAcceptorInterceptor;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Configuration
+@EnableRSocketSecurity
+public class RSocketSecurityConfig {
+
+//    private JwtConverterProperties jwtConverterProperties;
+//
+//    public RSocketSecurityConfig(JwtConverterProperties jwtConverterProperties) {
+//        this.jwtConverterProperties = jwtConverterProperties;
+//    }
+
+    private JwtConverter jwtConverter;
+
+    public RSocketSecurityConfig(JwtConverter jwtConverter) {
+        this.jwtConverter = jwtConverter;
+    }
+
+    @Bean
+    public PayloadSocketAcceptorInterceptor rsocketInterceptor(RSocketSecurity rsocket, ReactiveJwtDecoder jwtDecoder) {
+        rsocket.authorizePayload(authorize ->
+                        authorize
+                                .anyRequest().authenticated()
+                                .anyExchange().permitAll()
+                )
+                .jwt(jwtSpec -> jwtSpec.authenticationManager(jwtReactiveAuthenticationManager(jwtDecoder)));
+        return rsocket.build();
+    }
+
+    /**
+     * Spring Boot’s auto-configured ReactiveJwtDecoder only kicks in for HTTP security, not for RSocket security by default.
+     * @param issuerLocation issuerLocation.
+     * @return ReactiveJwtDecoder
+     */
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder(@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String issuerLocation) {
+        return ReactiveJwtDecoders.fromIssuerLocation(issuerLocation);
+    }
+
+    @Bean
+    public JwtReactiveAuthenticationManager jwtReactiveAuthenticationManager(ReactiveJwtDecoder decoder) {
+        JwtReactiveAuthenticationManager manager = new JwtReactiveAuthenticationManager(decoder);
+        manager.setJwtAuthenticationConverter(new ReactiveJwtAuthenticationConverterAdapter(this.jwtConverter));
+        return manager;
+    }
+}
