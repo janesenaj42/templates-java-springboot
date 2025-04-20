1398f2111ee3adcd661813e80f2b3313f3b188e5
janesenaj42 <janesoopy@gmail.com>
2025-04-20 22:56:20 +0800

    feat: add rsocket authentication and authorization

--- README.md
+++ README.md
@@ -11,9 +11,7 @@ This project uses keycloak for authentication. To set up keycloak, run
 ```declarative
 docker compose up -d
 ```
-Keycloak will be available at http://localhost:8080
-
-(TODO: include keycloak setup guide)
+Keycloak will be available at http://localhost:8080. Setup guide for keycloak is in [Keycloak Setup](#keycloak-setup).
 
 ### DevContainers
 If you're lazy to install Java, you may run the project in VS Code using DevContainers extention with the given `Dockerfile`.
@@ -55,8 +53,43 @@ curl --location 'http://localhost:8080/notifications' \
        
     // Request-stream
    rsc --stream --route=/request-stream --authBearer=$ACCESS_TOKEN --data="my sample input"  tcp://localhost:7000
-    ```
    
+   // Request-stream (requires 'administrator' client role)
+   rsc --stream --route=/admin.request-stream --authBearer=$ACCESS_TOKEN --data="my sample input"  tcp://localhost:7000
 
+   // Request-stream (requires 'user' client role)
+   rsc --stream --route=/admin.request-stream --authBearer=$ACCESS_TOKEN --data="my sample input"  tcp://localhost:7000
+    ```
+   You should get 'Access Denied' response if `--authBearer=$ACCESS_TOKEN` is not provided.
+   
+## Keycloak Setup
 
+1. Login to keycloak at http://localhost:8080 with the XX
+2. Create a new realm `my-realm`.
+![img.png](media/img.png)
+3. Navigate to `Clients` and create a new client `my-sample-app` with the following settings. Leave the other default settings.
+![img_1.png](media/img_1.png)
+![img_2.png](media/img_2.png)
+4. Navigate to `my-sample-app` client and add two new client roles (`administrator` and `user`):
+![img_3.png](media/img_3.png)
+5. Navigate to `Users` and create two new users (`adminuser` and `normaluser`). Click on each user and set password their password. Ensure the password is not temporary.
+![img_6.png](media/img_6.png)
+![img_5.png](media/img_5.png)
+6. Navigate to `Groups` and create two new groups (`admingroup` and `normalgroup`).
+![img_7.png](media/img_7.png)
+   1. Add the respective client roles created in step 4 to the respective groups (i.e. `administrator` is added to `admingroup`, `user` is added to `normalgroup`).
+      ![img_8.png](media/img_8.png)
+      ![img_9.png](media/img_9.png)
+      ![img_10.png](media/img_10.png)
+   2. Add the respective users created in step 5 to the respective groups (i.e. `adminuser` to `admingroup` and `normaluser` to `normalgroup`).
+      ![img_11.png](media/img_11.png)
+      ![img_12.png](media/img_12.png)
+      ![img_13.png](media/img_13.png)
+7. (Optional) If you followed step 5 to create users, ensure that the `Required actions` for the `Authentication` tab is as follows. If not, keyclaok will complain that the user setup is incomplete when retrieving the access token to test the endpoints.
+   ![img_14.png](media/img_14.png)
+8. To check that the users have been configured properly with the relevant client roles,
+   1. generate a token at http://localhost:8080/realms/my-realm/protocol/openid-connect/token. 
+      ![img_15.png](media/img_15.png)
+   2. Decode the generated jwt and ensure that the `resource_access` section has the correct client roles configured (sample output using https://jwt.io/).
+      ![img_16.png](media/img_16.png)
 
--- src/main/java/com/example/demo/config/RSocketSecurityConfig.java
+++ src/main/java/com/example/demo/config/RSocketSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,43 +23,65 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@Slf4j
 @Configuration
 @EnableRSocketSecurity
 public class RSocketSecurityConfig {
 
-    private JwtConverter jwtConverter;
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuerLocation;
 
-    public RSocketSecurityConfig(JwtConverter jwtConverter) {
-        this.jwtConverter = jwtConverter;
-    }
+    @Value("${jwt.auth.converter.resource-id}")
+    private String resourceId;
+
+    public static final String NORMAL_USER = "user";
+    public static final String ADMIN_USER = "administrator";
 
     @Bean
-    public PayloadSocketAcceptorInterceptor rsocketInterceptor(RSocketSecurity rsocket, JwtReactiveAuthenticationManager jwtAuthManager) {
+    public PayloadSocketAcceptorInterceptor rsocketInterceptor(RSocketSecurity rsocket) {
+        // `.anyExchange().permitAll()' allows non-payload-level exchanges like setup frames, authentication metadata, etc., so the client can actually connect and be authenticated.
         rsocket.authorizePayload(authorize ->
                         authorize
-                                .route("normal.*").hasAuthority("normaluser")
-                                .route("admin.*").hasAuthority("superuser")
+                                .route("/normal.*").hasRole(NORMAL_USER)
+                                .route("/admin.*").hasRole(ADMIN_USER)
                                 .anyRequest().authenticated()
                                 .anyExchange().permitAll()
                 )
-                .jwt(jwt -> jwt.authenticationManager(jwtAuthManager));
+                .jwt(jwtSpec -> jwtSpec.authenticationManager(jwtReactiveAuthenticationManager(jwtDecoder())));
         return rsocket.build();
     }
 
-    /**
-     * Spring Boot’s auto-configured ReactiveJwtDecoder only kicks in for HTTP security, not for RSocket security by default.
-     * @param issuerLocation issuerLocation.
-     * @return ReactiveJwtDecoder
-     */
     @Bean
-    public ReactiveJwtDecoder jwtDecoder(@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String issuerLocation) {
+    public ReactiveJwtDecoder jwtDecoder() {
         return ReactiveJwtDecoders.fromIssuerLocation(issuerLocation);
     }
 
     @Bean
     public JwtReactiveAuthenticationManager jwtReactiveAuthenticationManager(ReactiveJwtDecoder decoder) {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+            return Stream.concat(
+                    jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
+                    extractRoles(jwt).stream()
+            ).collect(Collectors.toSet());
+        });
         JwtReactiveAuthenticationManager manager = new JwtReactiveAuthenticationManager(decoder);
-        manager.setJwtAuthenticationConverter(new ReactiveJwtAuthenticationConverterAdapter(this.jwtConverter));
+        manager.setJwtAuthenticationConverter(new ReactiveJwtAuthenticationConverterAdapter(converter));
         return manager;
+    }
+
+    private Collection<? extends GrantedAuthority> extractRoles(Jwt jwt) {
+        Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+        Map<String, Object> resource;
+        Collection<String> resourceRoles;
+        if (resourceAccess == null
+                || (resource = (Map<String, Object>) resourceAccess.get(resourceId)) == null
+                || (resourceRoles = (Collection<String>) resource.get("roles")) == null) {
+            return Collections.emptySet();
+        }
+        return resourceRoles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toSet());
     }
 }
